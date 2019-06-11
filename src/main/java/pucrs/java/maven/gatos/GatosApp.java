01fbb5b29f946e569736225714e117fc31f2a8c7
Marco Mangan <marco.mangan@gmail.com>
2019-06-11 14:25:19 -0300

    #1

---
+++ src/main/java/pucrs/java/maven/gatos/GatosApp.java
@@ -0,0 +1,13 @@
+package pucrs.java.maven.gatos;
+
+/**
+ * Hello world!
+ *
+ */
+public class GatosApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
