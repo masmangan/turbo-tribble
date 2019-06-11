c164f07888d89f0c14d4e6e1da197148dcbd1f98
Marco Mangan <marco.mangan@gmail.com>
2019-06-11 14:34:18 -0300

    #1

--- src/test/java/pucrs/java/maven/gatos/GatosAppTest.java
+++ src/test/java/pucrs/java/maven/gatos/GatosAppTest.java
@@ -1,38 +1,16 @@
 package pucrs.java.maven.gatos;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class GatosAppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GatosAppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( GatosAppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class GatosAppTest {
+	/**
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
 }
